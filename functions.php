8d85f99d2d0989e9702d1237b36807bc8e89dc89
shaidulrony123 <shaidulislamrony1@gmail.com>
2024-12-23 00:21:29 +0600

    add ul li a class

--- functions.php
+++ functions.php
@@ -7,10 +7,56 @@ if(!function_exists('mythemefunction')){
     }
     load_theme_textdomain('neogymtextdomain', get_template_directory() . '/languages');
     //register new menu
-    register_nav_menus(array(
-        'top_menu' => __('Top Menu', 'neogymtextdomain'),
-    
-    ));
+        register_nav_menus(array(
+            'top_menu' => __('Top Menu', 'neogymtextdomain'),
+        
+        ));
+        // Add custom class to <ul>
+        if (!function_exists('custom_ul_class_add')) {
+            function custom_ul_class_add($args)
+            {
+                if (isset($args['add_ul_class'])) {
+                    $args['menu_class'] .= ' ' . $args['add_ul_class']; // Add custom class to <ul>
+                }
+                return $args;
+            }
+        }
+        add_filter('wp_nav_menu_args', 'custom_ul_class_add');
+
+// Add custom class to <li> and active class for Home or current page
+if (!function_exists('custom_li_class_add')) {
+    function custom_li_class_add($classes, $item, $args)
+    {
+        if (isset($args->add_li_class)) {
+            $classes[] = $args->add_li_class; // Add custom class to <li>
+        }
+
+        // Add "active" class to the current menu item
+        if (in_array('current-menu-item', $classes) || in_array('current_page_item', $classes)) {
+            $classes[] = 'active';
+        }
+
+        // Make Home active by default if no page is selected
+        if (is_front_page() && $item->url === home_url('/')) {
+            $classes[] = 'active';
+        }
+
+        return $classes;
+    }
+}
+add_filter('nav_menu_css_class', 'custom_li_class_add', 10, 3);
+
+// Add custom class to <a>
+if (!function_exists('custom_a_class_add')) {
+    function custom_a_class_add($atts, $item, $args)
+    {
+        if (isset($args->add_a_class)) {
+            $atts['class'] = $args->add_a_class; // Add custom class to <a>
+        }
+        return $atts;
+    }
+}
+add_filter('nav_menu_link_attributes', 'custom_a_class_add', 10, 3);
 }
 add_action('after_setup_theme', 'mythemefunction');
 ?>
--- header.php
+++ header.php
@@ -47,27 +47,19 @@
           <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
               <?php
-                wp_nav_menu(
-                  array(
+              // Render the menu
+              wp_nav_menu(
+                array(
                     'theme_location' => 'top_menu',
-                    
-                  ));
+                    'menu_class' => 'navbar-nav',       // Default class for <ul>
+                    'add_ul_class' => 'custom-ul-class', // Additional custom class for <ul>
+                    'add_li_class' => 'nav-item',       // Custom class for <li>
+                    'add_a_class' => 'nav-link',        // Custom class for <a>
+                )
+            );
+            
               ?>
-              <ul class="navbar-nav  ">
-                <li class="nav-item active">
-                  <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item ">
-                  <a class="nav-link" href="why.html"> Why us </a>
-                </li>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="trainer.html"> trainers</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="contact.html"> Contact Us</a>
-                </li>
-              </ul>
+           
               <div class="user_option">
                 <form class="form-inline my-2 my-lg-0 ml-0 ml-lg-4 mb-3 mb-lg-0">
                   <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit"></button>
